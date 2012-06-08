6a33cd11d22a4c93d0bfb0010af6557d10bb2009
venkcauk <venkcauk.dev@gmail.com>
2012-06-08 17:56:03 +0530

    Control Statment files

---
+++ ControlStatment/ControlStatment.java
@@ -0,0 +1,14 @@
+class ControlStatment {
+
+	public static void main(String args[]) {
+
+		System.out.println("-----Control Statment-----");
+
+		int a, b, c;
+
+		a = 10;
+		System.out.println("a = "+a);
+
+
+	}
+}
