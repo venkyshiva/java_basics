7496677a715ce28ffea328082c6cf3e10ec529a1
venkcauk <venkcauk.dev@gmail.com>
2012-06-09 08:29:15 +0530

    While/Do While Control statements

---
+++ WhileDemo/WhileDemo.java
@@ -0,0 +1,25 @@
+class WhileDemo { 
+
+	static boolean FIND  = true;
+
+	static int NUMBER = 10;
+      
+	public static void main(String args[]) {
+	
+		int i = 0;
+
+		while(i < 10) {
+		
+			System.out.println("i ="+ i);
+
+			i++;			
+		}
+
+		/*Infint while loop
+		 *	
+		while(FIND) {
+	
+			System.out.println("Find ="+ FIND);
+		} */ 
+	}
+}
